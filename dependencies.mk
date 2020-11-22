b709601cb1477e52054f69c37d8df80d39a101a4
sadko4u <sadko4u@gmail.com>
2020-11-22 22:48:36 +0300

    Preparing for release

--- dependencies.mk
+++ dependencies.mk
@@ -19,7 +19,7 @@
 #
 
 # Variables that describe dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.10
+LSP_COMMON_LIB_VERSION     := 1.0.11
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
@@ -29,12 +29,12 @@ LSP_DSP_LIB_NAME           := lsp-dsp-lib
 LSP_DSP_LIB_TYPE           := src
 LSP_DSP_LIB_URL            := https://github.com/sadko4u/$(LSP_DSP_LIB_NAME).git
 
-LSP_RUNTIME_LIB_VERSION    := 0.5.5
+LSP_RUNTIME_LIB_VERSION    := 0.5.6
 LSP_RUNTIME_LIB_NAME       := lsp-runtime-lib
 LSP_RUNTIME_LIB_TYPE       := src
 LSP_RUNTIME_LIB_URL        := https://github.com/sadko4u/$(LSP_RUNTIME_LIB_NAME).git
 
-LSP_LLTL_LIB_VERSION       := 0.5.4
+LSP_LLTL_LIB_VERSION       := 0.5.5
 LSP_LLTL_LIB_NAME          := lsp-lltl-lib
 LSP_LLTL_LIB_TYPE          := src
 LSP_LLTL_LIB_URL           := https://github.com/sadko4u/$(LSP_LLTL_LIB_NAME).git
