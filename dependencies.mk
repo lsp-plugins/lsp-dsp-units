159a2771d3f0f07949a2771335b627d0a1031a7a
sadko4u <sadko4u@gmail.com>
2020-12-15 01:39:53 +0300

    Added DynamicDelay DSP processing unit.

--- dependencies.mk
+++ dependencies.mk
@@ -39,7 +39,7 @@ LSP_LLTL_LIB_NAME          := lsp-lltl-lib
 LSP_LLTL_LIB_TYPE          := src
 LSP_LLTL_LIB_URL           := https://github.com/sadko4u/$(LSP_LLTL_LIB_NAME).git
 
-LSP_TEST_FW_VERSION        := 1.0.6
+LSP_TEST_FW_VERSION        := 1.0.7
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
