85f0f8c3e8c401b03cb6df8c4b8bd078ae5f07f2
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-08-06 17:22:52 +0300

    Fixed unintialized values issue

--- include/lsp-plug.in/dsp-units/noise/Generator.h
+++ include/lsp-plug.in/dsp-units/noise/Generator.h
@@ -105,8 +105,6 @@ namespace lsp
                 } color_params_t;
 
             private:
-                size_t              nSampleRate;
-
                 MLS                 sMLS;
                 LCG                 sLCG;
                 Velvet              sVelvetNoise;
@@ -117,6 +115,7 @@ namespace lsp
                 velvet_params_t     sVelvetParams;
                 color_params_t      sColorParams;
 
+                size_t              nSampleRate;
                 ng_generator_t      enGenerator;
 
                 float               fAmplitude;
