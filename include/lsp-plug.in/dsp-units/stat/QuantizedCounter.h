faffee1e99536f2197a6c03d86e95edc89187d30
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-11-14 02:38:51 +0300

    Implemented QuantizedCounter statistics tool

---
+++ include/lsp-plug.in/dsp-units/stat/QuantizedCounter.h
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2024 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2024 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-dsp-units
+ * Created on: 13 нояб. 2024 г.
+ *
+ * lsp-dsp-units is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-dsp-units is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-dsp-units. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LSP_PLUG_IN_DSP_UNITS_STAT_QUANTIZEDCOUNTER_H_
+#define LSP_PLUG_IN_DSP_UNITS_STAT_QUANTIZEDCOUNTER_H_
+
+#include <lsp-plug.in/dsp-units/version.h>
+
+#include <lsp-plug.in/common/status.h>
+#include <lsp-plug.in/dsp-units/iface/IStateDumper.h>
+
+namespace lsp
+{
+    namespace dspu
+    {
+        /**
+         * Counter that allows to compute distibution of some signal among it's values
+         */
+        class QuantizedCounter
+        {
+            private:
+                uint32_t        nPeriod;        // Duration for statistics
+                uint32_t        nLevels;        // Number of quantization levels
+                uint32_t        nHead;          // Position of the head in the history buffer
+                uint32_t        nCapacity;      // Overall capacity of the history buffer
+                uint32_t        nCount;         // Number of records in buffer
+                uint32_t        nMaxPeriod;     // Maximum possible period
+                uint32_t        nMaxLevels;     // Maximum possible levels
+                float           fMinValue;      // Minimum value
+                float           fMaxValue;      // Maximum value
+                float           fRStep;         // The reciprocal of quantization step
+                bool            bUpdate;        // Need to update computations
+
+                float          *vHistory;       // History buffer
+                uint32_t       *vCounters;      // History counters
+
+                uint8_t        *pData;
+
+            private:
+                void            inc_counters(const float *src, size_t count);
+                void            dec_counters(const float *src, size_t count);
+                void            evict_values();
+
+            public:
+                QuantizedCounter();
+                QuantizedCounter(const QuantizedCounter &) = delete;
+                QuantizedCounter(QuantizedCounter &&) = delete;
+                ~QuantizedCounter();
+
+                QuantizedCounter & operator = (const QuantizedCounter &) = delete;
+                QuantizedCounter & operator = (QuantizedCounter &&) = delete;
+
+                void            construct();
+                void            destroy();
+
+            public:
+                /**
+                 * Initialize counter
+                 * @param max_period maximum period (in samples)
+                 * @param max_levels maximum number of quantization levels
+                 * @return status of operation
+                 */
+                status_t        init(size_t max_period, size_t max_levels);
+
+            public:
+                /**
+                 * Set the statistics estimation period
+                 * @param period statistics estimation period
+                 */
+                void            set_period(size_t period);
+
+                /**
+                 * Get the statistics estimation period
+                 * @return statistics estimation period
+                 */
+                inline size_t   period() const          { return nPeriod;       }
+
+                /**
+                 * Get maximum possible statistics estimation period
+                 * @return maximum possible statistics estimation period
+                 */
+                inline size_t   max_period() const      { return nMaxPeriod;    }
+
+                /**
+                 * Set number of quantization levels
+                 * @param levels number of quantization levels
+                 */
+                void            set_levels(size_t levels);
+
+                inline size_t   levels() const          { return nLevels;       }
+
+                /**
+                 * Set minimum quantization range value
+                 * @param min minimum range value
+                 */
+                void            set_min_value(float min);
+
+                /**
+                 * Set maximum quantization range value
+                 * @param max maximum range value
+                 */
+                void            set_max_value(float max);
+
+                /**
+                 * Set minimum and maximum range values
+                 * @param min minimum range value
+                 * @param max maximum range value
+                 */
+                void            set_value_range(float min, float max);
+
+                /**
+                 * Set quantization range parameters
+                 * @param min minimum range value
+                 * @param max maximum range value
+                 * @param levels number of quantization levels
+                 */
+                void            set_range(float min, float max, size_t levels);
+
+                /**
+                 * Check if distribution counter needs to be updated
+                 * @return true if distribution counter needs to be updated
+                 */
+                inline bool     needs_update() const        { return bUpdate; }
+
+                /**
+                 * Update distribution counter settings
+                 */
+                void            update_settings();
+
+                /**
+                 * Process signal and update distribution counters
+                 * @param data data to process
+                 * @param count number of samples to process
+                 */
+                void            process(const float *data, size_t count);
+
+                /**
+                 * Clear the statistics
+                 */
+                void            clear();
+
+                /**
+                 * Get actual values of counters
+                 * @return actual values of counters
+                 */
+                inline const uint32_t *counters() const     { return vCounters;     }
+
+                /**
+                 * Get count of values that are below the quantization range
+                 * (the number of values that give the negative counter index)
+                 * @return count of values that are below the quantization range
+                 */
+                inline uint32_t below() const               { return vCounters[nMaxLevels];         }
+
+                /**
+                 * Get count of values that are above the quantization range
+                 * (the number of values that give the counter index not less then number of levels set)
+                 * @return count of values that are below the quantization range
+                 */
+                inline uint32_t above() const               { return vCounters[nMaxLevels + 1];     }
+
+                /**
+                 * Return number of samples used to compute counters
+                 * @return number of samples used to compute counters
+                 */
+                inline size_t   count() const               { return nCount;        }
+
+                /**
+                 * Dump the state
+                 * @param dumper dumper
+                 */
+                void            dump(IStateDumper *v) const;
+        };
+
+    } /* namespace dspu */
+} /* namespace lsp */
+
+#endif /* LSP_PLUG_IN_DSP_UNITS_STAT_QUANTIZEDCOUNTER_H_ */
---
+++ src/main/stat/QuantizedCounter.cpp
@@ -0,0 +1,290 @@
+/*
+ * Copyright (C) 2024 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2024 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-dsp-units
+ * Created on: 13 нояб. 2024 г.
+ *
+ * lsp-dsp-units is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-dsp-units is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-dsp-units. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <lsp-plug.in/common/alloc.h>
+#include <lsp-plug.in/dsp/dsp.h>
+#include <lsp-plug.in/dsp-units/stat/QuantizedCounter.h>
+
+namespace lsp
+{
+    namespace dspu
+    {
+        constexpr size_t BUFFER_GAP     = 0x400;
+
+        QuantizedCounter::QuantizedCounter()
+        {
+            construct();
+        }
+
+        QuantizedCounter::~QuantizedCounter()
+        {
+            destroy();
+        }
+
+        void QuantizedCounter::construct()
+        {
+            nPeriod         = 0;
+            nLevels         = 0;
+            nHead           = 0;
+            nCount          = 0;
+            nMaxPeriod      = 0;
+            nMaxLevels      = 0;
+
+            fMinValue       = 0.0f;
+            fMaxValue       = 0.0f;
+            fRStep          = 0.0f;
+
+            bUpdate         = true;
+
+            vHistory        = NULL;
+            vCounters       = NULL;
+
+            pData           = NULL;
+        }
+
+        void QuantizedCounter::destroy()
+        {
+            free_aligned(pData);
+
+            vHistory        = NULL;
+            vCounters       = NULL;
+        }
+
+        status_t QuantizedCounter::init(size_t max_period, size_t max_levels)
+        {
+            size_t szof_history     = align_size((max_period + BUFFER_GAP) * sizeof(float), DEFAULT_ALIGN);
+            size_t szof_levels     = align_size((max_levels + 2) * sizeof(uint32_t), DEFAULT_ALIGN);
+
+            uint8_t *data           = NULL;
+            uint8_t *ptr            = alloc_aligned<uint8_t>(data, szof_history + szof_levels);
+            if (ptr == NULL)
+                return STATUS_NO_MEM;
+
+            vHistory                = advance_ptr_bytes<float>(ptr, szof_history);
+            vCounters               = advance_ptr_bytes<uint32_t>(ptr, szof_levels);
+            nHead                   = 0;
+            nCapacity               = szof_history / sizeof(float);
+            nCount                  = 0;
+            nMaxPeriod              = max_period;
+            nMaxLevels              = max_levels;
+
+            dsp::fill_zero(vHistory, nCapacity);
+            for (size_t i=0; i<max_levels + 2; ++i)
+                vCounters[i]            = 0;
+
+            free_aligned(pData);
+            pData                   = data;
+
+            return STATUS_OK;
+        }
+
+        void QuantizedCounter::set_period(size_t period)
+        {
+            period          = lsp_limit(period, 1u, nMaxPeriod);
+            if (period == nPeriod)
+                return;
+
+            nPeriod         = period;
+        }
+
+        void QuantizedCounter::set_levels(size_t levels)
+        {
+            levels          = lsp_limit(levels, 1u, nMaxLevels);
+            if (levels == nLevels)
+                return;
+
+            nLevels         = levels;
+            bUpdate         = true;
+        }
+
+        void QuantizedCounter::set_min_value(float min)
+        {
+            if (fMinValue == min)
+                return;
+
+            fMinValue       = min;
+            bUpdate         = true;
+        }
+
+        void QuantizedCounter::set_max_value(float max)
+        {
+            if (fMaxValue == max)
+                return;
+
+            fMaxValue       = max;
+            bUpdate         = true;
+        }
+
+        void QuantizedCounter::set_value_range(float min, float max)
+        {
+            if ((fMinValue == min) &&
+                (fMaxValue == max))
+                return;
+
+            fMinValue       = min;
+            fMaxValue       = max;
+            bUpdate         = true;
+        }
+
+        void QuantizedCounter::set_range(float min, float max, size_t levels)
+        {
+            levels          = lsp_limit(levels, 1u, nMaxLevels);
+            if ((nLevels == levels) &&
+                (fMinValue == min) &&
+                (fMaxValue == max))
+                return;
+
+            nLevels         = levels;
+            fMinValue       = min;
+            fMaxValue       = max;
+            bUpdate         = true;
+        }
+
+        void QuantizedCounter::update_settings()
+        {
+            if (!bUpdate)
+                return;
+            bUpdate         = false;
+
+            // Update step and cleanup counters
+            fRStep          = float(nLevels) / (fMaxValue - fMinValue);
+            for (size_t i=0; i<nMaxLevels + 2; ++i)
+                vCounters[i]            = 0;
+
+            // Update counters according to the stored samples in the history buffer
+            if (nCount <= 0)
+                return;
+
+            size_t tail     = (nHead + nCapacity - nCount) % nCapacity;
+            for (size_t i=0; i<nCount; )
+            {
+                const size_t to_do  = lsp_min(nCount - i, nCapacity - tail);
+
+                inc_counters(&vHistory[tail], to_do);
+
+                i                  += to_do;
+                tail                = (tail + to_do) % nCapacity;
+            }
+        }
+
+        void QuantizedCounter::clear()
+        {
+            nHead           = 0;
+            nCount          = 0;
+
+            for (size_t i=0; i<nMaxLevels + 2; ++i)
+                vCounters[i]            = 0;
+        }
+
+        void QuantizedCounter::inc_counters(const float *src, size_t count)
+        {
+            const int32_t max_level     = nLevels;
+            for (size_t i=0; i<count; ++i)
+            {
+                int32_t index           = (src[i] - fMinValue) * fRStep;
+                if (index < 0)
+                    index                   = nMaxLevels;
+                else if (index >= max_level)
+                    index                   = nMaxLevels + 1;
+
+                ++vCounters[index];
+            }
+        }
+
+        void QuantizedCounter::dec_counters(const float *src, size_t count)
+        {
+            const int32_t max_level     = nLevels;
+            for (size_t i=0; i<count; ++i)
+            {
+                int32_t index           = (src[i] - fMinValue) * fRStep;
+                if (index < 0)
+                    index                   = nMaxLevels;
+                else if (index >= max_level)
+                    index                   = nMaxLevels + 1;
+
+                --vCounters[index];
+            }
+        }
+
+        void QuantizedCounter::evict_values()
+        {
+            size_t tail     = (nHead + nCapacity - nCount) % nCapacity;
+            while (nCount > nPeriod)
+            {
+                const size_t to_do  = lsp_min(nCount - nPeriod, nCapacity - tail);
+
+                dec_counters(&vHistory[tail], to_do);
+
+                nCount             -= to_do;
+                tail                = (tail + to_do) % nCapacity;
+            }
+        }
+
+        void QuantizedCounter::process(const float *data, size_t count)
+        {
+            // Update settings if necessary
+            update_settings();
+
+            // First, we need to evict values that exceed the maximum allowed period
+            evict_values();
+
+            for (size_t offset=0; offset < count; )
+            {
+                const size_t to_do  = lsp_min(count - offset, nCapacity - nHead);
+
+                // Put new values to the buffer and update statistics
+                dsp::copy(&vHistory[nHead], &data[offset], to_do);
+                inc_counters(&data[offset], to_do);
+                nCount             += to_do;
+                nHead               = (nHead + to_do) % nCapacity;
+
+                // Evict old values from the buffer
+                evict_values();
+
+                // Advance in cycle
+                offset             += to_do;
+            }
+        }
+
+        void QuantizedCounter::dump(IStateDumper *v) const
+        {
+            v->write("nPeriod", nPeriod);
+            v->write("nLevels", nLevels);
+            v->write("nHead", nHead);
+            v->write("nCapacity", nCapacity);
+            v->write("nCount", nCount);
+            v->write("nMaxPeriod", nMaxPeriod);
+            v->write("nMaxLevels", nMaxLevels);
+
+            v->write("fMinValue", fMinValue);
+            v->write("fMaxValue", fMaxValue);
+            v->write("fRStep", fRStep);
+            v->write("bUpdate", bUpdate);
+
+            v->write("vHistory", vHistory);
+            v->write("vCounters", vCounters);
+
+            v->write("pData", pData);
+        }
+
+    } /* namespace dspu */
+} /* namespace lsp */
+
