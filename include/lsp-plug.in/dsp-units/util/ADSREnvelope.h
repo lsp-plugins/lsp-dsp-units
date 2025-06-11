ab6c86d48e21d6f8bfb2ef22682aad2c214e7fae
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-06-12 00:54:26 +0300

    First results of line computations

--- include/lsp-plug.in/dsp-units/util/ADSREnvelope.h
+++ include/lsp-plug.in/dsp-units/util/ADSREnvelope.h
@@ -40,6 +40,7 @@ namespace lsp
             public:
                 enum function_t
                 {
+                    ADSR_NONE,
                     ADSR_LINE
                 };
 
@@ -61,21 +62,38 @@ namespace lsp
                     P_TOTAL
                 };
 
-                struct line_func_t
+                struct gen_none_t
                 {
+                    float               fT;
+                    float               fK;
+                    float               fB;
+                };
+
+                struct gen_line_t
+                {
+                    float               fT1;
+                    float               fT2;
                     float               fK1;
+                    float               fB1;
                     float               fK2;
                     float               fB2;
                 };
 
+                union gen_params_t
+                {
+                    gen_none_t          sNone;
+                    gen_line_t          sLine;
+                };
+
+                typedef float (*generator_t)(float t, const gen_params_t *params);
+
                 struct curve_t
                 {
-                    float               fEnd;
+                    float               fTime;
                     float               fCurve;
                     function_t          enFunction;
-                    union {
-                        line_func_t         sLine;
-                    };
+                    generator_t         pGenerator;
+                    gen_params_t        sParams;
                 };
 
             protected:
@@ -86,8 +104,17 @@ namespace lsp
                 uint32_t            nFlags;
 
             protected:
+                static float        none_generator(float t, const gen_params_t *params);
+                static float        line_generator(float t, const gen_params_t *params);
+                static inline float limit_range(float t, float prev);
+                static inline void  configure_curve(curve_t *curve, float x0, float x1, float y0, float y1);
+
+            protected:
                 void                set_param(float & param, float value);
                 void                set_function(function_t & func, function_t value);
+                void                set_flag(uint32_t flag, bool set);
+                void                set_curve(part_t part, float time, float curve, function_t func);
+                inline float        do_process(float value);
 
             public:
                 ADSREnvelope();
@@ -109,46 +136,56 @@ namespace lsp
                 void                destroy();
 
             public:
-                inline void         set_attack_time(float value)        { set_param(vCurve[P_ATTACK].fEnd, value);      }
-                inline void         set_hold_time(float value)          { set_param(fHoldTime, value);                  }
-                inline void         set_decay_time(float value)         { set_param(vCurve[P_DECAY].fEnd, value);       }
-                inline void         set_slope_time(float value)         { set_param(vCurve[P_SLOPE].fEnd, value);       }
-                inline void         set_relese_time(float value)        { set_param(vCurve[P_RELEASE].fEnd, value);     }
+                inline void         set_attack_time(float value)        { set_param(vCurve[P_ATTACK].fTime, value);         }
+                inline void         set_hold_time(float value)          { set_param(fHoldTime, value);                      }
+                inline void         set_decay_time(float value)         { set_param(vCurve[P_DECAY].fTime, value);          }
+                inline void         set_slope_time(float value)         { set_param(vCurve[P_SLOPE].fTime, value);          }
+                inline void         set_relese_time(float value)        { set_param(vCurve[P_RELEASE].fTime, value);        }
 
-                inline void         set_attack_curve(float value)       { set_param(vCurve[P_ATTACK].fCurve, value);    }
-                inline void         set_decay_curve(float value)        { set_param(vCurve[P_DECAY].fCurve, value);     }
-                inline void         set_slope_curve(float value)        { set_param(vCurve[P_SLOPE].fCurve, value);     }
-                inline void         set_release_curve(float value)      { set_param(vCurve[P_RELEASE].fCurve, value);   }
+                inline void         set_attack_curve(float value)       { set_param(vCurve[P_ATTACK].fCurve, value);        }
+                inline void         set_decay_curve(float value)        { set_param(vCurve[P_DECAY].fCurve, value);         }
+                inline void         set_slope_curve(float value)        { set_param(vCurve[P_SLOPE].fCurve, value);         }
+                inline void         set_release_curve(float value)      { set_param(vCurve[P_RELEASE].fCurve, value);       }
 
-                inline void         set_attack_function(float value)    { set_param(vCurve[P_ATTACK].fCurve, value);    }
-                inline void         set_decay_function(float value)     { set_param(vCurve[P_DECAY].fCurve, value);     }
-                inline void         set_slope_function(float value)     { set_param(vCurve[P_SLOPE].fCurve, value);     }
-                inline void         set_release_function(float value)   { set_param(vCurve[P_RELEASE].fCurve, value);   }
+                inline void         set_attack_function(function_t f)   { set_function(vCurve[P_ATTACK].enFunction, f);     }
+                inline void         set_decay_function(function_t f)    { set_function(vCurve[P_DECAY].enFunction, f);      }
+                inline void         set_slope_function(function_t f)    { set_function(vCurve[P_SLOPE].enFunction, f);      }
+                inline void         set_release_function(function_t f)  { set_function(vCurve[P_RELEASE].enFunction, f);    }
 
-                inline void         set_break_level(float value)        { set_param(fBreakLevel, value);                }
-                inline void         set_sustain_level(float value)      { set_param(fSustainLevel, value);              }
+                inline void         set_break_level(float value)        { set_param(fBreakLevel, value);                    }
+                inline void         set_sustain_level(float value)      { set_param(fSustainLevel, value);                  }
 
-                inline float        attack_time() const                 { return vCurve[P_ATTACK].fEnd;                 }
-                inline float        hold_time() const                   { return fHoldTime;                             }
-                inline float        decay_time() const                  { return vCurve[P_DECAY].fEnd;                  }
-                inline float        slope_time() const                  { return vCurve[P_SLOPE].fEnd;                  }
-                inline float        release_time() const                { return vCurve[P_RELEASE].fEnd;                }
+                inline void         set_hold_enabled(bool enabled)      { set_flag(F_USE_HOLD, enabled);                    }
+                inline void         set_break_enabled(bool enabled)     { set_flag(F_USE_BREAK, enabled);                   }
 
-                inline float        attack_curve() const                { return vCurve[P_ATTACK].fCurve;               }
-                inline float        decay_curve() const                 { return vCurve[P_DECAY].fCurve;                }
-                inline float        slope_curve() const                 { return vCurve[P_SLOPE].fCurve;                }
-                inline float        release_curve() const               { return vCurve[P_RELEASE].fCurve;              }
+                inline void         set_attack(float time, float curve, function_t func)    { set_curve(P_ATTACK, time, curve, func);   }
+                void                set_hold(float time, bool enabled);
+                inline void         set_decay(float time, float curve, function_t func)     { set_curve(P_DECAY, time, curve, func);    }
+                void                set_break(float level, bool enabled);
+                inline void         set_slope(float time, float curve, function_t func)     { set_curve(P_SLOPE, time, curve, func);    }
+                inline void         set_release(float time, float curve, function_t func)   { set_curve(P_RELEASE, time, curve, func);  }
 
-                inline function_t   attack_function() const             { return vCurve[P_ATTACK].enFunction;           }
-                inline function_t   decay_function() const              { return vCurve[P_DECAY].enFunction;            }
-                inline function_t   slope_function() const              { return vCurve[P_SLOPE].enFunction;            }
-                inline function_t   release_function() const            { return vCurve[P_RELEASE].enFunction;          }
+                inline float        attack_time() const                 { return vCurve[P_ATTACK].fTime;                    }
+                inline float        hold_time() const                   { return fHoldTime;                                 }
+                inline float        decay_time() const                  { return vCurve[P_DECAY].fTime;                     }
+                inline float        slope_time() const                  { return vCurve[P_SLOPE].fTime;                     }
+                inline float        release_time() const                { return vCurve[P_RELEASE].fTime;                   }
 
-                inline float        break_level() const                 { return fBreakLevel;                           }
-                inline float        sustain_level() const               { return fSustainLevel;                         }
+                inline float        attack_curve() const                { return vCurve[P_ATTACK].fCurve;                   }
+                inline float        decay_curve() const                 { return vCurve[P_DECAY].fCurve;                    }
+                inline float        slope_curve() const                 { return vCurve[P_SLOPE].fCurve;                    }
+                inline float        release_curve() const               { return vCurve[P_RELEASE].fCurve;                  }
 
-                inline float        hold_enabled() const                { return nFlags & F_USE_HOLD;                   }
-                inline float        break_enabled() const               { return nFlags & F_USE_BREAK;                  }
+                inline function_t   attack_function() const             { return vCurve[P_ATTACK].enFunction;               }
+                inline function_t   decay_function() const              { return vCurve[P_DECAY].enFunction;                }
+                inline function_t   slope_function() const              { return vCurve[P_SLOPE].enFunction;                }
+                inline function_t   release_function() const            { return vCurve[P_RELEASE].enFunction;              }
+
+                inline float        break_level() const                 { return fBreakLevel;                               }
+                inline float        sustain_level() const               { return fSustainLevel;                             }
+
+                inline float        hold_enabled() const                { return nFlags & F_USE_HOLD;                       }
+                inline float        break_enabled() const               { return nFlags & F_USE_BREAK;                      }
 
             public:
                 /**
@@ -157,11 +194,27 @@ namespace lsp
                 void            update_settings();
 
                 /**
-                 * Compute ADSR point
+                 * Compute ADSR point for specified value
                  * @param value normalized time point [0..1]
                  * @return computed ADSR value or 0 if outside the [0..1] range
                  */
-                float           compute(float value);
+                float           process(float value);
+
+                /**
+                 * Compute ADSR points for specified values
+                 * @param dst destination buffer to store result
+                 * @param src source values to process
+                 * @param count number of values to process
+                 */
+                void            process(float *dst, const float *src, size_t count);
+
+                /**
+                 * Compute amd apply ADSR points for specified values and apply to target buffer
+                 * @param dst destination buffer to apply result
+                 * @param src source values to process
+                 * @param count number of values to process
+                 */
+                void            process_mul(float *dst, const float *src, size_t count);
 
                 /**
                  * Generate part of the ADSR curve and store to buffer
@@ -173,13 +226,13 @@ namespace lsp
                 void            generate(float *dst, float start, float step, size_t count);
 
                 /**
-                 * Apply curve to the buffer
+                 * Generate and apply curve to the buffer
                  * @param dst destination buffer to modify
                  * @param start the time of first point
                  * @param step the actual time step
                  * @param count number of points to apply
                  */
-                void            apply(float *dst, float start, float step, size_t count);
+                void            generate_mul(float *dst, float start, float step, size_t count);
 
                 /**
                  * Apply curve to the buffer
@@ -189,7 +242,7 @@ namespace lsp
                  * @param step the actual time step
                  * @param count number of points to apply
                  */
-                void            apply(float *dst, const float *src, float start, float step, size_t count);
+                void            generate_mul(float *dst, const float *src, float start, float step, size_t count);
 
                 /**
                  * Dump the state
--- src/main/util/ADSREnvelope.cpp
+++ src/main/util/ADSREnvelope.cpp
@@ -41,12 +41,12 @@ namespace lsp
             {
                 curve_t *cv     = &vCurve[i];
 
-                cv->fEnd        = 0.0f;
-                cv->fCurve      = 0.5f;
-                cv->enFunction  = ADSR_LINE;
-                cv->sLine.fK1   = 0.0f;
-                cv->sLine.fK2   = 0.0f;
-                cv->sLine.fB2   = 0.0f;
+                cv->fTime               = 0.0f;
+                cv->fCurve              = 0.5f;
+                cv->enFunction          = ADSR_NONE;
+                cv->pGenerator          = none_generator;
+                cv->sParams.sNone.fK    = 0.0f;
+                cv->sParams.sNone.fB    = 0.0f;
             }
 
             fHoldTime       = 0.0f;
@@ -79,40 +79,268 @@ namespace lsp
             nFlags         |= F_RECONFIGURE;
         }
 
+        void ADSREnvelope::set_flag(uint32_t flag, bool set)
+        {
+            bool on                 = nFlags & flag;
+            if (set != on)
+                nFlags                  = lsp_setflag(nFlags, flag, set) | F_RECONFIGURE;
+        }
+
+        void ADSREnvelope::set_curve(part_t part, float time, float curve, function_t func)
+        {
+            curve_t *cv             = &vCurve[part];
+
+            time                    = lsp_limit(time, 0.0f, 1.0f);
+            curve                   = lsp_limit(curve, 0.0f, 1.0f);
+
+            if ((cv->fTime == time) && (cv->fCurve == curve) && (cv->enFunction == func))
+                return;
+
+            cv->fTime               = time;
+            cv->fCurve              = curve;
+            cv->enFunction          = func;
+            nFlags                 |= F_RECONFIGURE;
+        }
+
+        void ADSREnvelope::set_hold(float time, bool enabled)
+        {
+            set_param(fHoldTime, time);
+            set_flag(F_USE_HOLD, enabled);
+        }
+
+        void ADSREnvelope::set_break(float level, bool enabled)
+        {
+            set_param(fBreakLevel, level);
+            set_flag(F_USE_BREAK, enabled);
+        }
+
+        inline float ADSREnvelope::limit_range(float t, float prev)
+        {
+            return lsp_limit(lsp_max(t, prev), 0.0f, 1.0f);
+        }
+
+        void ADSREnvelope::configure_curve(curve_t *curve, float x0, float x1, float y0, float y1)
+        {
+            switch (curve->enFunction)
+            {
+                case ADSR_LINE:
+                {
+                    curve->pGenerator       = line_generator;
+                    gen_line_t *g           = &curve->sParams.sLine;
+
+                    g->fT1                  = x0;
+                    g->fT2                  = 0.5f * (x0 + x1);
+                    const float cy          = y0 + curve->fCurve * (y1 - y0);
+
+                    g->fK1                  = (cy - y0) / (g->fT2 - x0);
+                    g->fB1                  = y0;
+
+                    g->fK2                  = (y1 - cy) / (x1 - g->fT2);
+                    g->fB2                  = cy;
+                    break;
+                }
+
+                case ADSR_NONE:
+                default:
+                {
+                    curve->pGenerator       = none_generator;
+                    gen_none_t *g           = &curve->sParams.sNone;
+
+                    g->fT                   = x0;
+                    g->fK                   = (y1 - y0) / (x1 - x0);
+                    g->fB                   = y0;
+                    break;
+                }
+            }
+        }
+
         void ADSREnvelope::update_settings()
         {
             if (!(nFlags & F_RECONFIGURE))
                 return;
 
-            // TODO: compute curve parameters
+            // Update time ranges
+            vCurve[P_ATTACK].fTime  = limit_range(vCurve[P_ATTACK].fTime, 0.0f);
+            if (nFlags & F_USE_HOLD)
+            {
+                fHoldTime               = limit_range(fHoldTime, vCurve[P_ATTACK].fTime);
+                vCurve[P_DECAY].fTime   = limit_range(vCurve[P_DECAY].fTime, fHoldTime);
+            }
+            else
+                vCurve[P_DECAY].fTime   = limit_range(vCurve[P_DECAY].fTime, vCurve[P_ATTACK].fTime);
+
+            if (nFlags & F_USE_BREAK)
+            {
+                vCurve[P_SLOPE].fTime   = limit_range(vCurve[P_SLOPE].fTime, vCurve[P_DECAY].fTime);
+                vCurve[P_RELEASE].fTime = limit_range(vCurve[P_RELEASE].fTime, vCurve[P_SLOPE].fTime);
+            }
+            else
+                vCurve[P_RELEASE].fTime = limit_range(vCurve[P_RELEASE].fTime, vCurve[P_DECAY].fTime);
+
+            // Configure curves
+            configure_curve(
+                &vCurve[P_ATTACK],
+                0.0f, vCurve[P_ATTACK].fTime,
+                0.0f, 1.0f);
+            const float hold        = (nFlags & F_USE_HOLD) ? fHoldTime : vCurve[P_ATTACK].fTime;
+            const float decay       = vCurve[P_DECAY].fTime;
+            if (nFlags & F_USE_BREAK)
+            {
+                configure_curve(
+                    &vCurve[P_DECAY],
+                    hold, decay,
+                    1.0f, fBreakLevel);
+                configure_curve(
+                    &vCurve[P_SLOPE],
+                    decay, vCurve[P_SLOPE].fTime,
+                    fBreakLevel, fSustainLevel);
+            }
+            else
+            {
+                configure_curve(
+                    &vCurve[P_DECAY],
+                    hold, decay,
+                    1.0f, fSustainLevel);
+            }
+
+            configure_curve(
+                &vCurve[P_RELEASE],
+                vCurve[P_RELEASE].fTime, 1.0f,
+                fSustainLevel, 0.0f);
 
             nFlags         &= ~F_RECONFIGURE;
         }
 
-        float ADSREnvelope::compute(float value)
+        float ADSREnvelope::do_process(float t)
+        {
+            if ((t < 0.0f) || (t > 1.0f))
+                return 0.0f;
+
+            // Attack
+            const curve_t *cv   = &vCurve[P_ATTACK];
+            if (t < cv->fTime)
+                return cv->pGenerator(t, &cv->sParams);
+            const float hold    = (nFlags & F_USE_HOLD) ? fHoldTime : cv->fTime;
+
+            // Hold
+            if (t < hold)
+                return 1.0f;
+
+            // Decay
+            cv                  = &vCurve[P_DECAY];
+            if (t < cv->fTime)
+                return cv->pGenerator(t, &cv->sParams);
+
+            // Slope
+            if (nFlags & F_USE_BREAK)
+            {
+                cv                  = &vCurve[P_SLOPE];
+                if (t < cv->fTime)
+                    return cv->pGenerator(t, &cv->sParams);
+            }
+
+            // Sustain and Release
+            cv          = &vCurve[P_RELEASE];
+            return (t < cv->fTime) ? fSustainLevel : cv->pGenerator(t, &cv->sParams);
+        }
+
+        float ADSREnvelope::process(float value)
+        {
+            update_settings();
+            return do_process(value);
+        }
+
+        void ADSREnvelope::process(float *dst, const float *src, size_t count)
         {
             update_settings();
 
-            // TODO
-            return 0.0f;
+            for (size_t i=0; i<count; ++i)
+                dst[i]      = do_process(src[i]);
+        }
+
+        void ADSREnvelope::process_mul(float *dst, const float *src, size_t count)
+        {
+            update_settings();
+
+            for (size_t i=0; i<count; ++i)
+                dst[i]     *= do_process(src[i]);
+        }
+
+        float ADSREnvelope::none_generator(float t, const gen_params_t *params)
+        {
+            const gen_none_t *none = &params->sNone;
+            return (t - none->fT) * none->fK + none->fB;
+        }
+
+        float ADSREnvelope::line_generator(float t, const gen_params_t *params)
+        {
+            const gen_line_t *line = &params->sLine;
+            return (t < line->fT2) ?
+                (t - line->fT1) * line->fK1 + line->fB1 :
+                (t - line->fT2) * line->fK2 + line->fB2;
         }
 
         void ADSREnvelope::generate(float *dst, float start, float step, size_t count)
         {
             update_settings();
 
-            // TODO
+            size_t i            = 0;
+            float t             = start;
+
+            // Time before attack
+            for ( ; (t < 0.0f) && (i < count); t = start + i * step)
+                dst[i++]            = 0.0f;
+
+            // Attack segment
+            const curve_t *cv   = &vCurve[P_ATTACK];
+            for ( ; (t < cv->fTime) && (i < count); t = start + i * step)
+                dst[i++]            = cv->pGenerator(t, &cv->sParams);
+
+            // Hold segment
+            if (nFlags & F_USE_HOLD)
+            {
+                for ( ; (t < fHoldTime) && (i < count); t = start + i * step)
+                    dst[i++]            = 1.0f;
+            }
+
+            // Decay segment
+            cv                  = &vCurve[P_DECAY];
+            for ( ; (t < cv->fTime) && (i < count); t = start + i * step)
+                dst[i++]            = cv->pGenerator(t, &cv->sParams);
+
+            // Slope segment
+            if (nFlags & F_USE_BREAK)
+            {
+                cv                  = &vCurve[P_SLOPE];
+                for ( ; (t < cv->fTime) && (i < count); t = start + i * step)
+                    dst[i++]            = cv->pGenerator(t, &cv->sParams);
+            }
+
+            // Sustain segment
+            cv                  = &vCurve[P_RELEASE];
+            for ( ; (t < cv->fTime) && (i < count); t = start + i * step)
+                dst[i++]            = fSustainLevel;
+
+            // Release segment
+            for ( ; (t <= 1.0f) && (i < count); t = start + i * step)
+                dst[i++]            = cv->pGenerator(t, &cv->sParams);
+
+            // Time after release
+            for ( ; (i < count); t = start + i * step)
+                dst[i++]            = 0.0f;
         }
 
-        void ADSREnvelope::apply(float *dst, float start, float step, size_t count)
+        void ADSREnvelope::generate_mul(float *dst, float start, float step, size_t count)
         {
             update_settings();
 
             // TODO
         }
 
-        void ADSREnvelope::apply(float *dst, const float *src, float start, float step, size_t count)
+        void ADSREnvelope::generate_mul(float *dst, const float *src, float start, float step, size_t count)
         {
+            update_settings();
+
             // TODO
         }
 
