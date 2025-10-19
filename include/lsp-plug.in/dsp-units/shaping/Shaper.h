1ae578fe5e18a454e3e1e646649353291a3e352f
Stefano Tronci <tronci.stefano@gmail.com>
2025-10-19 11:54:27 +0100

    Redesign of Shaper based on parameters that are common to each shaping function.

--- include/lsp-plug.in/dsp-units/shaping/Shaper.h
+++ include/lsp-plug.in/dsp-units/shaping/Shaper.h
@@ -52,37 +52,81 @@ namespace lsp
         {
             enum update_t
             {
-                UPD_SINUSOIDAL              = 1 << 0,
-                UPD_POLYNOMIAL              = 1 << 1,
-                UPD_HYPERBOLIC              = 1 << 2,
-                UPD_EXPONENTIAL             = 1 << 3,
-                UPD_POWER                   = 1 << 4,
-                UPD_BILINEAR                = 1 << 5,
-                UPD_ASYMMETRIC_CLIP         = 1 << 6,
-                UPD_ASYMMETRIC_SOFTCLIP     = 1 << 7,
-                UPD_QUARTER_CYCLE           = 1 << 8,
-                UPD_RECTIFIER               = 1 << 9,
-                UPD_BITCRUSH_FLOOR          = 1 << 10,
-                UPD_BITCRUSH_CEIL           = 1 << 11,
-                UPD_BITCRUSH_ROUND          = 1 << 12,
-                UPD_TAP_TUBEWARMTH          = 1 << 13,
+                UPD_SLOPE       = 1 << 0,
+                UPD_SHAPE       = 1 << 1,
+                UPD_HIGH_LEVEL  = 1 << 2,
+                UPD_LOW_LEVEL   = 1 << 3,
+                UPD_RADIUS      = 1 << 4,
+                UPD_LEVELS      = 1 << 5,
+                UPD_DRIVE       = 1 << 6,
+                UPD_BLEND       = 1 << 7,
 
                 UPD_ALL =
-                        UPD_SINUSOIDAL | UPD_POLYNOMIAL |
-                        UPD_HYPERBOLIC | UPD_EXPONENTIAL |
-                        UPD_BILINEAR | UPD_ASYMMETRIC_CLIP |
-                        UPD_ASYMMETRIC_SOFTCLIP | UPD_QUARTER_CYCLE |
-                        UPD_RECTIFIER | UPD_BITCRUSH_FLOOR |
-                        UPD_BITCRUSH_CEIL | UPD_BITCRUSH_ROUND |
-                        UPD_TAP_TUBEWARMTH
+                        UPD_SLOPE |
+                        UPD_SHAPE |
+                        UPD_HIGH_LEVEL | UPD_LOW_LEVEL |
+                        UPD_RADIUS |
+                        UPD_LEVELS |
+                        UPD_DRIVE | UPD_BLEND
             };
 
             private:
-                dspu::shaping::shaping_t    sParams;
+                sh_function_t               enFunction;
+                dspu::shaping::shaping_t    sShaping;
+
+                /**
+                 * Parameters cross-section: These are in common among all shaping functions.
+                 * They might need re-scaling and shifting.
+                 */
+
+                /**
+                 * [0, 1], The following functions need a rescaled and/or shifted version of this:
+                 * - SH_FCN_SINUSOIDAL
+                 */
+                float                       fSlope;
+
+                /**
+                 * [0, 1], The following functions need a rescaled and/or shifted version of this:
+                 * - SH_FCN_POLYNOMIAL
+                 * - SH_FCN_HYPERBOLIC
+                 * - SH_FCN_EXPONENTIAL
+                 * - SH_FCN_POWER
+                 * - SH_FCN_BILINEAR
+                 * - SH_FCN_RECTIFIER
+                 */
+                float                       fShape;
+
+                /**
+                 * [0, 1], The following functions need a rescaled and/or shifted version of these:
+                 * - SH_FCN_ASYMMETRIC_CLIP
+                 * - SH_FCN_ASYMMETRIC_SOFTCLIP
+                 */
+                float                       fHighLevel;
+                float                       fLowLevel;
+
+                /**
+                 * [0, 1], The following functions need a rescaled and/or shifted version of this:
+                 * - SH_FCN_QUARTER_CYCLE
+                 */
+                float                       fRadius;
+
+                /**
+                 * [0, 1], The following functions need a rescaled and/or shifted version of this:
+                 * - SH_FCN_BITCRUSH_FLOOR
+                 * - SH_FCN_BITCRUSH_CEIL
+                 * - SH_FCN_BITCRUSH_ROUND
+                 */
+                float                       fLevels;
+
+                /**
+                 * [0, 1], The following functions need a rescaled and/or shifted version of these:
+                 * - SH_FCN_TAP_TUBEWARMTH
+                 */
+                float                       fDrive;
+                float                       fBlend;
 
                 size_t                      nSampleRate;
-                sh_function_t               enFunction;
-                size_t                      nUpdate;
+                uint32_t                    nUpdateFlags;
 
             public:
                 explicit Shaper();
@@ -97,6 +141,7 @@ namespace lsp
                 void destroy();
 
             protected:
+                void needs_update() const;
                 void update_settings();
 
             public:
@@ -112,101 +157,53 @@ namespace lsp
                  */
                 void set_sample_rate(size_t sr);
 
-                /** Set the sinusoidal function slope.
+                /** Set the slope parameter.
                  *
                  * @param slope slope
                  */
-                void set_sinusoidal_slope(float slope);
+                void set_slope(float slope);
 
-                /** Set the polynomial function shape.
+                /** Set the shape parameter.
                  *
                  * @param shape shape
                  */
-                void set_polynomial_shape(float shape);
+                void set_shape(float shape);
 
-                /** Set the hyperbolic function shape.
+                /** Set the high level parameter.
                  *
-                 * @param shape shape
+                 * @param high_level high level
                  */
-                void set_hyperbolic_shape(float shape);
+                void set_high_level(float high_level);
 
-                /** Set the exponential function shape.
+                /** Set the low level parameter.
                  *
-                 * @param shape shape
+                 * @param low_level low level
                  */
-                void set_exponential_shape(float shape);
+                void set_low_level(float low_level);
 
-                /** Set the bilinear function shape.
-                 *
-                 * @param shape shape
-                 */
-                void set_bilinear_shape(float shape);
-
-                /** Set the asymmetric clip function high clip level.
-                 *
-                 * @param high_clip high clip level
-                 */
-                void set_asymmetric_clip_high_clip(float high_clip);
-
-                /** Set the asymmetric clip function low clip level.
-                 *
-                 * @param low_clip low clip level
-                 */
-                void set_asymmetric_clip_low_clip(float low_clip);
-
-                /** Set the asymmetric softclip function high clip limit level.
-                 *
-                 * @param high_limit high clip limit level
-                 */
-                void set_asymmetric_softclip_high_limit(float high_limit);
-
-                /** Set the asymmetric softclip function low clip limit level.
-                 *
-                 * @param low_limit low clip limit level
-                 */
-                void set_asymmetric_softclip_low_limit(float low_limit);
-
-                /** Set the quarter circle function radius.
+                /** Set the radius parameter.
                  *
                  * @param radius radius
                  */
-                void set_quarter_circle_radius(float radius);
+                void set_radius(float radius);
 
-                /** Set the rectifier function shape.
-                 *
-                 * @param shape shape
-                 */
-                void set_rectifier_shape(float shape);
-
-                /** Set the bitcrush floor function levels.
+                /** Set the levels parameter.
                  *
                  * @param levels levels
                  */
-                void set_bitcrush_floor_levels(float levels);
+                void set_levels(float levels);
 
-                /** Set the bitcrush ceil function levels.
-                 *
-                 * @param levels levels
-                 */
-                void set_bitcrush_ceil_levels(float levels);
-
-                /** Set the bitcrush round function levels.
-                 *
-                 * @param levels levels
-                 */
-                void set_bitcrush_round_levels(float levels);
-
-                /** Set the TAP Tubewarmth function drive.
+                /** Set the drive.
                  *
                  * @param drive drive
                  */
-                void set_tap_tubewarmth_drive(float drive);
+                void set_drive(float drive);
 
-                /** Set the TAP Tubewarmth function blend.
+                /** Set the blend parameter.
                  *
                  * @param blend blend
                  */
-                void set_tap_tubewarmth_blend(float blend);
+                void set_blend(float blend);
 
                 /** Output sequence to the destination buffer in additive mode
                  *
