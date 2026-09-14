179744bc29f4c472218cd865cea37d555e8a3220
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-09-14 22:18:11 +0300

    Refactoring of the Convolver module

--- include/lsp-plug.in/dsp-units/util/Convolver.h
+++ include/lsp-plug.in/dsp-units/util/Convolver.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-dsp-units
  * Created on: 29 янв. 2016 г.
@@ -25,33 +25,38 @@
 #include <lsp-plug.in/dsp-units/version.h>
 #include <lsp-plug.in/dsp-units/iface/IStateDumper.h>
 
-#define CONVOLVER_RANK_MIN          8                               /* buffer of 256 samples (128 effective)    */
-#define CONVOLVER_RANK_MAX          16                              /* buffer of 8192 samples (4096 effective)  */
+#define CONVOLVER_RANK_MIN          8                               /* buffer of 256 samples (128 effective)        */
+#define CONVOLVER_RANK_MAX          16                              /* buffer of 62256 samples (32768 effective)    */
 
 namespace lsp
 {
     namespace dspu
     {
+        /**
+         * Real-time convolver module. Performs hybrid convolution using FFT and direct implementation
+         * to reach zero latency output.
+         */
         class LSP_DSP_UNITS_PUBLIC Convolver
         {
             private:
                 float          *vDataBuffer;            // Buffer for storing convolution tail data
-                float          *vFrame;                 // Pointer to the beginning of the input data frame
+                float          *vFrameStart;            // Start of the frame
+                float          *vFrame;                 // Current pointer to the beginning of the input data frame
                 float          *vConvBuffer;            // Convolution buffer to perform convolution
                 float          *vTaskData;              // Task data for tail convolution
                 float          *vConvData;              // FFT convolution data
                 float          *vDirectData;            // Direct convolution data
 
-                size_t          nDataBufferSize;        // Size of data buffer
-                size_t          nDirectSize;            // Size of direct convolution data
-                size_t          nFrameSize;             // Size of input data frame
-                size_t          nFrameOff;              // Offset from the beginning of the input data frame
-                size_t          nConvSize;              // The actual convolution size in samples
-                size_t          nLevels;                // Number of raising convolution levels
-                size_t          nBlocks;                // Number of constant-size blocks
-                size_t          nBlocksDone;            // Number of applied constant-size blocks
-                size_t          nRank;                  // The actual rank of the convolution
-                size_t          nBlkInit;               // Initial number of blocks to apply at step # 0
+                uint32_t        nDataBufferSize;        // Size of data buffer
+                uint32_t        nDirectSize;            // Size of direct convolution data
+                uint32_t        nFrameSize;             // Size of input data frame
+                uint32_t        nFrameOff;              // Offset from the beginning of the input data frame
+                uint32_t        nConvSize;              // The actual convolution size in samples
+                uint32_t        nLevels;                // Number of raising convolution levels
+                uint32_t        nBlocks;                // Number of constant-size blocks
+                uint32_t        nBlocksDone;            // Number of applied constant-size blocks
+                uint32_t        nRank;                  // The actual rank of the convolution
+                uint32_t        nBlkInit;               // Initial number of blocks to apply at step # 0
                 float           fBlkCoef;               // The actual coefficient to compute proper block number per formula
 
                 uint8_t        *vData;                  // Non-aligned pointer to the whole allocated data
@@ -77,7 +82,9 @@ namespace lsp
 
             public:
 
-                /** Initialize convolver
+                /**
+                 * Initialize convolver. Performs memory reallocation if passed rank
+                 * or convolution plan differs to the previous setup.
                  *
                  * @param data convolution data
                  * @param count number of samples in convolution
@@ -93,6 +100,11 @@ namespace lsp
                  * @param count number of samples to process
                  */
                 void process(float *dst, const float *src, size_t count);
+
+                /**
+                 * Cleanup convolver state (internal memory)
+                 */
+                void clear();
 
                 /** Get the actual convolution size in samples
                  *
